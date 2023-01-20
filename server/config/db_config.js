d16b341a24d98408e955aca9af4cd0a7587f8129
Erielle Joeshua Bancolita <eriellebancolita017@gmail.com>
2023-01-20 16:44:52 +0800

    Client initialize

--- server/config/db_config.js
+++ server/config/db_config.js
@@ -1,3 +1,10 @@
+/* 
+  max: maximum number of connection in pool
+  min: minimum number of connection in pool
+  idle: maximum time, in milliseconds, that a connection can be idle before being released
+  acquire: maximum time, in milliseconds, that pool will try to get connection before throwing error 
+*/
+
 module.exports = {
   HOST: "localhost",
   USER: "root",
