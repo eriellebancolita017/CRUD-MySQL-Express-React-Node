2d836384fc91c1c7af2b8265610ac6f7036acdac
Erielle Joeshua Bancolita <eriellebancolita017@gmail.com>
2023-01-23 16:04:25 +0800

    Create tutorials and comments by using node command

--- server/models/index.js
+++ server/models/index.js
@@ -20,5 +20,6 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.tutorials = require("./tutorial_model")(sequelize, Sequelize);
+db.comments = require("./tutorial_comment")(sequelize, Sequelize);
 
 module.exports = db;
